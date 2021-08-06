349bfbba2a8ddbffa80cd6846af47d6ed8025755
Mohammad-Sa-2021 <you@example.com>
2021-08-06 19:58:33 +0300

    comiit 3

---
+++ bin/loop_practice.dart
@@ -0,0 +1,35 @@
+void main(List<String> args) {
+  //
+  var info = [
+    {
+      'name': 'Mohammad',
+      'age': 20,
+      'phone': 123456,
+    },
+    {
+      'name': 'Khaled',
+      'age': 25,
+      'phone': 987654,
+      'children': ['ch1', 'ch2', 'ch3'],
+    },
+    {
+      'name': 'Noor',
+      'age': 30,
+      'phone': 6532145,
+      'children': ['ch1', 'ch2', 'ch3'],
+    },
+  ];
+
+  for (var i = 0; i < info.length; i++) {
+    print(info[i]['children']);
+  }
+
+  //
+  // for (var i = 0; i < info.length; i++) {
+  //   print(info[i]['name']);
+  //   for (var j = 0; j < info[i]['children']; j++) {
+  //     print(info[i]['children'][j]);
+  //   }
+  //   print('-----------------------');
+  // }
+}
